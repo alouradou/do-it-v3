cb4f7f367eab979016add8346423994223ed6cc8
Arthur L <18040415+alouradou@users.noreply.github.com>
2024-10-15 14:13:02 +0200

    Fixed project's "résumé" bug

--- config/markdown/shortcodes/resume.js
+++ config/markdown/shortcodes/resume.js
@@ -1,34 +1,36 @@
-export default async function(eleventyConfig) {
+function extractExcerpt(content) {
+  // Original https://keepinguptodate.com/pages/2019/06/creating-blog-with-eleventy/
+  // Mis à jour et commenté en 11ty version 3.0
 
-  eleventyConfig.addShortcode('résumé', (arg) => extractExcerpt(arg))
-};
-
-function extractExcerpt(article) {
-  // https://keepinguptodate.com/pages/2019/06/creating-blog-with-eleventy/
-
-  if (!article.hasOwnProperty('templateContent')) {
-    console.warn('Failed to extract excerpt: Document has no property "templateContent".');
+  // Vérification si le contenu est bien fourni
+  if (!content) {
+    console.warn('Failed to extract excerpt: No content provided.');
     return null;
   }
- 
+
   let excerpt = null;
-  const content = article.templateContent;
- 
-  // The start and end separators to try and match to extract the excerpt
+
+  // Les séparateurs à utiliser pour identifier l'extrait dans le contenu
   const separatorsList = [
     { start: '<!-- début résumé -->', end: '<!-- fin résumé -->' },
     { start: '<p>', end: '</p>' }
   ];
- 
+
+  // Parcours des séparateurs et extraction de l'extrait
   separatorsList.some(separators => {
     const startPosition = content.indexOf(separators.start);
     const endPosition = content.indexOf(separators.end);
- 
+
     if (startPosition !== -1 && endPosition !== -1) {
       excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
-      return true; // Exit out of array loop on first match
+      return true; // On arrête la boucle sur la première correspondance trouvée
     }
   });
- 
-  return excerpt;
+
+  // Retourne l'extrait ou null si aucun extrait n'a été trouvé
+  return excerpt || null;
 }
+
+export default function(eleventyConfig) {
+  eleventyConfig.addAsyncShortcode('résumé', (arg) => extractExcerpt(arg))
+};
